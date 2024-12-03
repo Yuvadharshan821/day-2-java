2a02d93d0975d21f6bceadf43be7bb63504a7ac1
Yuvadharshan821 <yuvadharshan2005@gmail.com>
2024-12-03 13:36:42 +0530

    Add files via upload

---
+++ bitwise.java
@@ -0,0 +1,12 @@
+public class bitwiseor
+{
+    public static void main(String[] args)
+    {
+        System.out.println(11|18);
+        System.out.println(22|144);
+        System.out.println(88|122);
+        System.out.println(11&27);
+        System.out.println(88&122);
+        System.out.println(18&12);
+    }
+}
