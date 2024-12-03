ce956f68cfb72d385ffdcfa33e1b628b76f53ba7
Yuvadharshan821 <yuvadharshan2005@gmail.com>
2024-12-03 10:29:49 +0530

    Add files via upload

---
+++ arthemetic.java
@@ -0,0 +1,12 @@
+public class arthemetic
+{
+    public static void main(String[] args)
+    {
+        int a=10;
+        int b=5;
+       int sum=a-b;
+       int sub=a+b;
+       System.out.println(sum);
+       System.out.println(sub);
+}
+}
