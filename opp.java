f1f082077e75268788593d703e6769fc4102d8ef
Yuvadharshan821 <yuvadharshan2005@gmail.com>
2024-12-03 10:20:12 +0530

    Add files via upload

---
+++ opp.java
@@ -0,0 +1,9 @@
+public class opp
+{
+    public static void main(String[] args){
+        int a=45;
+        int b=25;
+        System.out.println(a==b);
+        System.out.println(a!=b);
+
+    }    }
