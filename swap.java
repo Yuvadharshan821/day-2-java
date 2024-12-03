f0576ae3d97441b2cd27720c9cbb274a5e0ac6f8
Yuvadharshan821 <yuvadharshan2005@gmail.com>
2024-12-03 11:11:10 +0530

    Add files via upload

---
+++ swap.java
@@ -0,0 +1,10 @@
+public class swap
+{
+    public static void main(String[] args)
+    {
+        int boys=50;
+        int girls=20;
+        boys=girls;
+        System.out.println(boys);
+    }
+}
